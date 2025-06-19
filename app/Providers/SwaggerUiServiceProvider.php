bc5c38ace18b41fe0991da0c0af819b4c38fc55f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-06-19 14:40:41 -0400

    refactor: update SwaggerUiServiceProvider for strict types and use arrow function; adjust phpstan and phpunit configurations to exclude Providers directory

--- app/Providers/SwaggerUiServiceProvider.php
+++ app/Providers/SwaggerUiServiceProvider.php
@@ -1,18 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
-class SwaggerUiServiceProvider extends ServiceProvider
+final class SwaggerUiServiceProvider extends ServiceProvider
 {
-    public function boot() : void
+    public function boot(): void
     {
-        Gate::define('viewSwaggerUI', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
-        });
+        Gate::define('viewSwaggerUI', fn ($user = null): bool => in_array(optional($user)->email, [
+            //
+        ]));
     }
 }
