b2095b7216611adb79b66252e63fba7133269bd2
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-06-19 13:46:50 -0400

    refactor: simplify product creation in DeleteProductActionTest by removing currency assignment

--- tests/Unit/Actions/DeleteProductActionTest.php
+++ tests/Unit/Actions/DeleteProductActionTest.php
@@ -3,13 +3,10 @@
 declare(strict_types=1);
 
 use App\Actions\DeleteProductAction;
-use App\Models\Currency;
 use App\Models\Product;
 
 test('it deletes a product', function () {
-    $product = Product::factory()->create(
-        ['currency_id' => Currency::factory()->create()->id]
-    );
+    $product = Product::factory()->create();
 
     $productId = $product->id;
 
